0d95920348238a4b37fd51611dc931e9e81ac3ee
Amon Apolonio <amonapolonio.dev@gmail.com>
2025-05-15 21:55:28 -0300

    feat: W03 Learning Activities

---
+++ utilities/index.js
@@ -0,0 +1,57 @@
+const invModel = require("../models/inventory-model")
+const Util = {}
+
+/* ************************
+ * Constructs the nav HTML unordered list
+ ************************** */
+Util.getNav = async function (req, res, next) {
+  let data = await invModel.getClassifications()
+  let list = "<ul>"
+  list += '<li><a href="/" title="Home page">Home</a></li>'
+  data.rows.forEach((row) => {
+    list += "<li>"
+    list +=
+      '<a href="/inv/type/' +
+      row.classification_id +
+      '" title="See our inventory of ' +
+      row.classification_name +
+      ' vehicles">' +
+      row.classification_name +
+      "</a>"
+    list += "</li>"
+  })
+  list += "</ul>"
+  return list
+}
+
+/* **************************************
+* Build the classification view HTML
+* ************************************ */
+Util.buildClassificationGrid = async function(data){
+  let grid = '';
+  if(data.length > 0){
+    grid = '<div id="inv-display" class="classification-grid">';
+    data.forEach(vehicle => { 
+      grid += '<div class="classification-item">';
+      grid += '<a href="../../inv/detail/'+ vehicle.inv_id 
+      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+      + 'details"><img src="' + vehicle.inv_thumbnail 
+      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +' on CSE Motors" /></a>';
+      grid += '<div class="vehicle-info">';
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" class="vehicle-name" title="View ' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>';
+      grid += '<span class="vehicle-price">$' 
+      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>';
+      grid += '</div>';
+      grid += '</div>';
+    });
+    grid += '</div>';
+  } else { 
+    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>';
+  }
+  return grid;
+}
+
+module.exports = Util
